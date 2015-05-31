7476df052d0bf9f68d6f2b16e87fe4b87a27b5f0
Ruurd Pels <rfpels@gmail.com>
2015-05-31 19:08:37 +0200

    Result of first lecture - basically following the footsteps

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var currentlyTypingANumber: Bool = false
+    
+    @IBOutlet weak var display: UILabel!
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if currentlyTypingANumber == true {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            currentlyTypingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
